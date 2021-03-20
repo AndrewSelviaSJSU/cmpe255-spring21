263a3c13dc14d6f587d24a60093b2842aa3595f2
Andrew Selvia <andrew.selvia@sjsu.edu>
2021-03-20 05:10:49 -0700

    Add lab3 & refactor one-hot-encoding

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,57 +1,172 @@
 import pandas as pd
-
+import itertools
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn import svm
+from keras import layers
+from keras import models
 
 
 class DiabetesClassifier:
   def __init__(self) -> None:
-    col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
-    self.pima = pd.read_csv('diabetes.csv', header=0, names=col_names, usecols=col_names)
+    self.columns = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+    self.pima = pd.read_csv('diabetes.csv', header=0, names=self.columns, usecols=self.columns)
     print(self.pima.head())
     self.X_test = None
     self.y_test = None
 
-  def define_feature(self):
-    feature_cols = ['pregnant', 'insulin', 'bmi', 'age']
-    X = self.pima[feature_cols]
-    y = self.pima.label
+  def define_feature(self, features, data=None):
+    if data is None:
+      data = self.pima
+    X = data[features]
+    y = data.label
     return X, y
 
-  def train(self):
-    # split X and y into training and testing sets
-    X, y = self.define_feature()
+  def train(self, X, y):
     X_train, self.X_test, y_train, self.y_test = train_test_split(X, y, random_state=0)
     # train a logistic regression model on the training set
     logistic_regression = LogisticRegression()
     logistic_regression.fit(X_train, y_train)
     return logistic_regression
 
-  def predict(self):
-    model = self.train()
+  def predict(self, X, y):
+    model = self.train(X, y)
     y_pred_class = model.predict(self.X_test)
     return y_pred_class
 
   def calculate_accuracy(self, result):
     return metrics.accuracy_score(self.y_test, result)
 
-  def examine(self):
-    dist = self.y_test.value_counts()
-    print(dist)
-    percent_of_ones = self.y_test.mean()
-    percent_of_zeros = 1 - self.y_test.mean()
-    return self.y_test.mean()
+  # def examine(self):
+  #   dist = self.y_test.value_counts()
+  #   print(dist)
+  #   percent_of_ones = self.y_test.mean()
+  #   percent_of_zeros = 1 - self.y_test.mean()
+  #   return self.y_test.mean()
 
   def confusion_matrix(self, result):
     return metrics.confusion_matrix(self.y_test, result)
 
 
-if __name__ == "__main__":
+def baseline():
   classifier = DiabetesClassifier()
-  result = classifier.predict()
+  X, y = classifier.define_feature(['pregnant', 'insulin', 'bmi', 'age'])
+  result = classifier.predict(X, y)
   print(f"Prediction={result}")
   score = classifier.calculate_accuracy(result)
   print(f"score={score}")
   con_matrix = classifier.confusion_matrix(result)
-  print(f"confusion_matrix=${con_matrix}")
+  print(f"confusion_matrix={con_matrix}")
+
+
+# https://stackoverflow.com/a/64320524/6073927
+def create_power_set(iterable):
+  for sl in itertools.product(*[[[], [i]] for i in iterable]):
+    yield {j for i in sl for j in i}
+
+
+def power_set_helper(feature_set, classifier, X, y):
+  print(f"feature set: {feature_set}")
+  X, y = classifier.define_feature(feature_set)
+  result = classifier.predict(X, y)
+  # print(f"Prediction={result}")
+  score = classifier.calculate_accuracy(result)
+  print(f"score={score}")
+  con_matrix = classifier.confusion_matrix(result)
+  print(f"confusion_matrix={con_matrix}")
+  return feature_set, score
+
+
+def solution_1():
+  classifier = DiabetesClassifier()
+  possible_features = classifier.columns
+  possible_features.remove("label")
+  X, y = classifier.define_feature(possible_features)
+  power_set = list(create_power_set(possible_features))
+  power_set.remove(set())
+  scores = list(map(lambda feature_set: power_set_helper(feature_set, classifier, X, y), power_set))
+  scores.sort(key=lambda x: x[1], reverse=True)
+  print(scores)
+
+
+def solution_4():
+  classifier = DiabetesClassifier()
+  best_features = ['pregnant', 'glucose', 'bp', 'bmi']
+  X, y = classifier.define_feature(best_features)
+  X = StandardScaler().fit_transform(X[best_features])
+  result = classifier.predict(X, y)
+  score = classifier.calculate_accuracy(result)
+  print(f"score={score}")
+  con_matrix = classifier.confusion_matrix(result)
+  print(f"confusion_matrix={con_matrix}")
+
+
+def solution_2():
+  classifier = DiabetesClassifier()
+  X, y = classifier.define_feature(['bp', 'bmi', 'pregnant', 'glucose'])
+  # X, y = classifier.define_feature(['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age'])
+  X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+  C_2d_range = [1e-2, 1, 1e2]
+  gamma_2d_range = [1e-1, 1, 1e1]
+  for C in C_2d_range:
+    for gamma in gamma_2d_range:
+      clf = svm.SVC(kernel='rbf', C=C, gamma=gamma)
+      clf.fit(X_train, y_train)
+      y_pred = clf.predict(X_test)
+      print(f"C: {C}")
+      print(f"gamma: {gamma}")
+      print(metrics.accuracy_score(y_test, y_pred))
+
+
+def solution_3():
+  classifier = DiabetesClassifier()
+  X, y = classifier.define_feature(['bp', 'bmi', 'pregnant', 'glucose'])
+  # X, y = classifier.define_feature(['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age'])
+  X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+  clf = svm.LinearSVC(max_iter=1000000)
+  clf.fit(X_train, y_train)
+  y_pred = clf.predict(X_test)
+  # con_matrix = clf.confusion_matrix(y_pred)
+  print(metrics.accuracy_score(y_test, y_pred))
+  # print(f"confusion_matrix={con_matrix}")
+
+
+def solution_5():
+  classifier = DiabetesClassifier()
+  best_features = ['pregnant', 'glucose', 'bp', 'bmi']
+  data = classifier.pima
+  data = data[data.bp != 0]
+  X, y = classifier.define_feature(best_features, data)
+  X = StandardScaler().fit_transform(X[best_features])
+  result = classifier.predict(X, y)
+  score = classifier.calculate_accuracy(result)
+  print(f"score={score}")
+  con_matrix = classifier.confusion_matrix(result)
+  print(f"confusion_matrix={con_matrix}")
+
+
+def solution_6():
+  classifier = DiabetesClassifier()
+  best_features = ['pregnant', 'glucose', 'bp', 'bmi']
+  data = classifier.pima
+  data = data[data.glucose != 0]
+  X, y = classifier.define_feature(best_features, data)
+  X = StandardScaler().fit_transform(X[best_features])
+  result = classifier.predict(X, y)
+  score = classifier.calculate_accuracy(result)
+  print(f"score={score}")
+  con_matrix = classifier.confusion_matrix(result)
+  print(f"confusion_matrix={con_matrix}")
+
+
+if __name__ == "__main__":
+  # baseline()
+  # solution_1()
+  # solution_2()
+  # solution_3()
+  # solution_4()
+  # solution_5()
+  solution_6()
--- lab3/output.md
+++ lab3/output.md
@@ -1,6 +1,41 @@
-| Experiement | Accuracy | Confusion Matrix | Comment |
-|-------------|----------|------------------|---------|
-| Baseline    | 0.6770833333333334 | [[114  16] [ 46  16]] |  |
-| Solution 1   | xxxxxxx  | xxxxxx |  xxxxx |
-| Solution 2   | xxxxxxx  | xxxxxx |  xxxxx |
-| Solution 3   | xxxxxxx  | xxxxxx |  xxxxx |
+# Lab 3
+
+| Experiment | Accuracy |   Confusion Matrix    | Comment                                                                                  |
+|------------|----------|-----------------------|------------------------------------------------------------------------------------------|
+| Baseline   |    0.677 | [[114  16] [ 46  16]] | LogisticRegression; {'pregnant', 'insulin', 'bmi', 'age'}                                |
+| Solution 1 |    0.812 | [[119  11] [ 25  37]] | LogisticRegression; {'glucose', 'pregnant', 'bp', 'bmi'}                                 |
+| Solution 2 |    0.682 | N/A                   | SVM                                                                                      |
+| Solution 3 |    0.812 | N/A                   | Linear SVC                                                                               |
+| Solution 4 |    0.812 | [[119  11] [ 25  37]] | StandardScaler + LogisticRegression; {'glucose', 'pregnant', 'bp', 'bmi'}                |
+| Solution 5 |    0.744 | [[105  12] [ 35  32]] | bp != 0 + StandardScaler + LogisticRegression; {'glucose', 'pregnant', 'bp', 'bmi'}      |
+| Solution 6 |    0.754 | [[110  18] [ 29  34]] | glucose != 0 + StandardScaler + LogisticRegression; {'glucose', 'pregnant', 'bp', 'bmi'} |
+
+The approaches I have taken are broken down into sections below:
+
+## Baseline
+
+This is what was initially provided. It trains a LogisticRegression model with the following features: {'pregnant', 'insulin', 'bmi', 'age'}. It simply establishes a baseline accuracy of 0.812
+
+## Solution 1
+
+The first thing I tried was to iterate over the power set of all possible features. I trained a LogisticRegression model identically to the baseline but with each iterative feature set. The feature set which produced the highest accuracy (0.812) was: {'glucose', 'pregnant', 'bp', 'bmi'}. 
+
+## Solution 2
+
+Next, I wanted to try a SVM model, since I learned about them last semester and figured they might be effective. I used an rbf kernel and tried various hyper-parameters, but no result improved upon Solution 1. Unfortunately, SVMs also lack a confusion matrix function.
+
+## Solution 3
+
+After reading through the scikit-learn documentation on SVMs, I suspected a Linear SVC might be an interesting approach to take. It just matched Solution 1, though.
+
+## Solution 4
+
+I came back to LogisticRegression to try feature engineering approaches rather than different models. I used a StandardScaler, but it didn't make a difference on top of Solution 1.
+
+## Solution 5
+
+I tried to remove outliers where `bp` == 0, but it actually made the model perform worse.
+
+## Solution 6
+
+This time, I tried removing outliers where `glucose` == 0, but it also made the model perform worse.
